5fc6618600e001b6b84437dd5889ed64b9302c64
Francis <francis@aichat.com>
2020-04-28 11:53:44 +0800

    fix: change default bot agant id

--- claimSubmission/frontend/build/config.js
+++ claimSubmission/frontend/build/config.js
@@ -1,1 +1,1 @@
-window.API_HOST = 'http://www.google.com'; window.DIALOGFLOW_AGENT_ID = 'ce617fac-032a-442b-8976-36b110b58133'
+window.API_HOST = 'http://127.0.0.1:5000'; window.DIALOGFLOW_AGENT_ID = 'f100c550-d81c-4f7f-952b-c2690d1c933f'
