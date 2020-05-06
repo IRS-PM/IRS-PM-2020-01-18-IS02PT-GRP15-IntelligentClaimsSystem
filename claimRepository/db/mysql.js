537b4839a54f888162a9b5947ad6d58b452da3c3
Francis <francis@aichat.com>
2020-05-07 00:17:07 +0800

    refactor: remove logging from claim repo

--- claimRepository/db/mysql.js
+++ claimRepository/db/mysql.js
@@ -4,7 +4,8 @@ const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS } = process.env
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
   port: DB_PORT,
-  dialect: 'mysql'
+  dialect: 'mysql',
+  logging: false
 })
 
 function connect() {  
