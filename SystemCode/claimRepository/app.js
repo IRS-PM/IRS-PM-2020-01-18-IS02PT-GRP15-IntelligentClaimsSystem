347c651c0e1ecf8aa47b23bf87d8a44451fae904
elisach <elisach@gmail.com>
2020-05-10 20:34:09 +0800

    fix: claimRepository buffer

--- SystemCode/claimRepository/app.js
+++ SystemCode/claimRepository/app.js
@@ -31,7 +31,8 @@ const init = async () => {
 
     // start http server
     const app = express()
-    app.use(express.json())
+    //app.use(express.json())
+    app.use(express.json({limit: '50mb'}));
     app.use(cors())
 
     app.use('/productplan', productPlanRoutes)
